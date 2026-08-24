07100c930fe6c7f42929667714159c8756b62a8a
nicko4o <216985190+nicko4o@users.noreply.github.com>
2026-08-24 14:47:41 +0800

    chore(release): bump version to 0.21.1

--- polars_baseball/__init__.py
+++ polars_baseball/__init__.py
@@ -29,7 +29,7 @@ from polars_baseball.enums.fangraphs import (
 )
 from polars_baseball.enums.position import Position
 
-__version__ = "0.21.0"
+__version__ = "0.21.1"
 
 
 _logging.getLogger("polars_baseball").addHandler(_logging.NullHandler())
